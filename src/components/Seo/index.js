3df8d8ceb2a1762f36b91958dcb437b9dd94b30b
Jhonatan Lima <jhonatan.vinicius.lima@gmail.com>
2021-03-03 15:50:57 -0300

    adjust

--- src/components/Seo/index.js
+++ src/components/Seo/index.js
@@ -4,10 +4,10 @@ import { Helmet } from "react-helmet";
 function Seo() {
   return (
     <Helmet>
-      <title>oi gabriel</title>
-      <meta name="description" content="Helmet application" />
+      <title>TESTANDO</title>
+      {/* <meta name="description" content="Helmet application" /> */}
       <meta property="og:image" content="https://image.shutterstock.com/z/stock-vector-background-for-social-media-proportions-of-the-web-banner-is-x-px-vector-1030361545.jpg" />
-      <meta property="og:image:type" content="image/jpg" />
+      <meta property="og:image:type" content="image/png" />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
 
